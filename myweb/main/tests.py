33102ec9b6e51666b82d210d951498206554a3c0
HBlackk <47453310+HBlackk@users.noreply.github.com>
2020-05-23 14:32:40 +0100

    Add files via upload

--- myweb/main/tests.py
+++ myweb/main/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
 
-# Create your tests here.
+import unittest
+import views
+from main.forms import InputName
+
+class viewsTesting(unittest.TestCase):
+
+        #tests that return is not none.
+        def test_graphVisual(self):
+            result = views.graph_visual()
+            self.assertIsNotNone()
+
+        #tests that return is not none.
+        def test_pieVisual(self):
+            result = views.pie_visual()
+            self.assertIsNotNone()
+
+        #tests that return is not none.
+        def test_graphVisual(self):
+            result = views.graph_visual()
+            self.assertIsNotNone()
+
+        #tests to see if remove_links returns true
+        def test_removeLinks(self):
+            result = views.remove_links()
+            self.assertTrue()
+
+        def test_removeLinks(self):
+            sample_tweet = tweet('This is a link: @www.harrisonblack.com/rewards')
+
+            self.assertEqual(sample.remove_links, 'This is a link: ')    
+
+if __name__ == '__main__':
+    unittest.main()
